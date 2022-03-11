d27ea73593f41cabb66032ae4abb9b80b1f813fa
Milla Tran <99290923+milla-tran@users.noreply.github.com>
2022-03-11 00:32:24 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,4 +1,3 @@
-
 library(shiny)
 library(shinyWidgets)
 library(shinythemes)
@@ -15,14 +14,14 @@ Stocks <- tq_exchange("NASDAQ")
 
 tickers2 <- paste(Stocks$symbol, "-", Stocks$company)
 
-#tickers <- c("AAPL","MSFT","GOOG","AMZN","TSLA","PYPL","EA")
 
-# Define UI for application that draws a histogram
+
+# Define UI for application that draws a graphic
 ui <- fluidPage(
-  theme = bslib::bs_theme(bg = "#010779",
+  theme = bslib::bs_theme(bg = "black",
                           fg = "white",
                           base_font = "Source Sans Pro"),
-  img(src = 'stockgraphic.jpg', align = "center"),
+
   
   # Application title
   titlePanel("Stock Prices"),
@@ -31,23 +30,7 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       width = 3,
-      
-      # Let user pick stocks
-      # pickerInput(
-      #   inputId = "stocks",
-      #   label = h4("Please select the stocks you would like to study"),
-      #   choices = c(
-      #     "Apple"           = tickers[1],
-      #     "Microsoft"       = tickers[2],
-      #     "Google"          = tickers[3],
-      #     "Amazon"          = tickers[4],
-      #     "Tesla"           = tickers[5],
-      #     "Paypal"          = tickers[6],
-      #     "Electronic Arts" = tickers[7]),
-      #   selected = tickers,
-      #   options = list(`actions-box` = TRUE),
-      #   multiple = T
-      # ),
+
       
       selectInput(
         inputId = 'stocks',
@@ -59,18 +42,6 @@ ui <- fluidPage(
         #options = list(create = FALSE) # if TRUE, allows newly created inputs
       ),
       
-      # dateRangeInput(inputId = "date", 
-      #                label = "Select date range", 
-      #                start = today()-months(12),
-      #                end = today(), 
-      #                min = today()-months(240),
-      #                max = today(), 
-      #                format = "yyyy-mm-dd", 
-      #                startview = "month", 
-      #                weekstart = 0,
-      #                language = "en", 
-      #                separator = " to ", 
-      #                width = NULL),
       
       dateInput(inputId = "date1", label = "Select start date", 
                 value = today()-months(12), 
@@ -81,7 +52,7 @@ ui <- fluidPage(
                 weekstart = 0,
                 language = "en", width = NULL),
       
-      dateInput(inputId = "date2", label = "Select start date", 
+      dateInput(inputId = "date2", label = "Select end date", 
                 value = today(), 
                 min = today()-months(240), 
                 max = today(),
@@ -95,31 +66,26 @@ ui <- fluidPage(
     ),
     
     # Plot results
-    mainPanel(#plotlyOutput("plot", height = 800),),
+    mainPanel(
       tabsetPanel(
         tabPanel(
           "Price Highs", 
           plotOutput("highs", height = 600)
-        )),
-      tabsetPanel(
+        ),
+        
         tabPanel(
           "Closing Prices",  
-      plotOutput('plot'))
+          plotOutput('plot', height = 600),
+          )
+      
+      )
+    ),
   )
-)))
+)
 
 server <- function(input, output) {
   
   observe({
-    
-    # prices <- tq_get(
-    #   input$stocks,
-    #   get  = "stock.prices",
-    #   from = today() - months(12),
-    #   to   = today(),
-    #   complete_cases = F
-    # ) %>%
-    #   select(symbol, date, close)
     
     if(is.null(input$stocks) == TRUE){
       prices <- tq_get(
@@ -133,15 +99,6 @@ server <- function(input, output) {
     }
     
     if(is.null(input$stocks) == FALSE){
-      # input$stocks <- separate(input$stocks,
-      #                            col = input$stocks[,1],
-      #                            into = c(ticker,name),
-      #                            sep = " - ",
-      #                            remove = TRUE,
-      #                            convert = FALSE,
-      #                            extra = "warn",
-      #                            fill = "warn"
-      # )
       
       tickers3 <- str_split_fixed(input$stocks, " - ", 2)
       
@@ -155,91 +112,49 @@ server <- function(input, output) {
         select(symbol, date, close, high)
     }
     
-    #autoplot(prices)
     
     output$plot <- 
-      
-      #renderPlot({   
-      #plot_stock <- input$stocks==Stocks[,1]
-      #autoplot(plot_stock, .vars = close)
-      
       renderPlot(
         prices %>%
           group_by(symbol) %>%
           ggplot(aes(x = date, y = close, color = symbol)) +
           geom_line(size=1) +
-          labs(title = "Stock Line Chart", y = "Closing Price", x = "", caption = "If blank, stock ticker is not valid") +
-          theme_fivethirtyeight())
+          labs(title = "Stock Line Chart", y = "Closing Price", x = "Date", caption = "If blank, stock ticker is not valid") +
+          theme(plot.title = element_text(size = 35, colour = "white", family = "Source Sans Pro"),
+                axis.title = element_blank(),
+                axis.title.x = element_text(size = 20, colour = "white", family = "Source Sans Pro"),
+                axis.title.y = element_text(size = 20, colour = "white", family = "Source Sans Pro"),
+                legend.title = element_text(colour="black", size=15),
+                plot.background = element_rect(fill = "black"),
+                panel.background = element_rect(fill="black"),
+                panel.grid = element_blank(),
+                legend.text = element_text(colour="white", family = "Source Sans Pro"),
+                legend.background = element_rect(fill = "black"),
+                legend.key = element_blank())
+        )
     
     output$highs <- renderPlot(
       prices %>%
         group_by(symbol) %>%
         ggplot(aes(x = date, y = high, color = symbol)) + 
         geom_line(size=1, alpha = .9) +
-        #theme_minimal(base_size=16) +
         labs(title = "Stock Price Highs Over Time", y = "Price", x = "Date", caption = "If blank, stock ticker is not valid") +
-        theme_fivethirtyeight()
-        # theme(plot.title = element_text(size = 35, colour = "white", family = "Lucida Sans"),
-        #       axis.title = element_blank(),
-        #       axis.title.x = element_text(size = 20, colour = "white", family = "Lucida Sans"),
-        #       axis.title.y = element_text(size = 20, colour = "white", family = "Lucida Sans"),
-        #       legend.title = element_text(colour="#010779", size=15),
-        #       plot.background = element_rect(fill = "#010779"),
-        #       panel.background = element_rect(fill="#010779"),
-        #       panel.grid = element_blank(),
-        #       legend.text = element_text(colour="white", family = "Lucida Sans"))
-    )
+        theme(plot.title = element_text(size = 35, colour = "white", family = "Source Sans Pro"),
+              axis.title = element_blank(),
+              axis.title.x = element_text(size = 20, colour = "white", family = "Source Sans Pro"),
+              axis.title.y = element_text(size = 20, colour = "white", family = "Source Sans Pro"),
+              legend.title = element_text(colour="black", size=15),
+              plot.background = element_rect(fill = "black"),
+              panel.background = element_rect(fill="black"),
+              panel.grid = element_blank(),
+              legend.text = element_text(colour="white", family = "Source Sans Pro"),
+              legend.background = element_rect(fill = "black"),
+              legend.key = element_blank())
+        )
     
-    # output$highs <- renderPlotly({
-    #   ggplotly(prices %>% filter(symbol %in% input$stocks) %>% 
-    #              ggplot(aes(date, high, colour = symbol)) +
-    #              geom_line(size = 1, alpha = .9) +
-    #              theme_minimal(base_size=16) +
-    #              labs(title = "Stock Price Highs over Time", x = "Date", y = "Price") +
-    #              theme(plot.title = element_text(size = 35, colour = "white", family = "Source Sans Pro"),
-    #                    axis.title = element_blank(),
-    #                    axis.title.x = element_text(size = 20, colour = "white", family = "Source Sans Pro"),
-    #                    axis.title.y = element_text(size = 20, colour = "white", family = "Source Sans Pro"),
-    #                    legend.title = element_text(colour="#010779", size=15),
-    #                    plot.background = element_rect(fill = "#010779"),
-    #                    panel.background = element_rect(fill="#010779"),
-    #                    panel.grid = element_blank(),
-    #                    legend.text = element_text(colour="white", family = "Source Sans Pro"))
-    #   )
-    # })
-    
-    # renderPlot(  
-    #   ggplot(data = prices, aes(x = date, y = close, group = symbol, color = "red")) + 
-    #     geom_point() + theme_bw() )
-    
-    # create output
-    # output$plot <- renderPlotly({
-    #   print(
-    #     ggplotly(
-    #       prices %>%
-    #         group_by(symbol) %>%
-    #         mutate(init_close = if_else(date == min(date), close, NA_real_)) %>%
-    #         mutate(value = round(
-    #           100 * close / sum(init_close, na.rm = T), 1
-    #         )) %>%
-    #         ungroup() %>%
-    #         ggplot(aes(date, value, colour = symbol)) +
-    #         geom_line(size = 1, alpha = .9) +
-    #         theme_minimal(base_size = 16) +
-    #         theme(
-    #           axis.title = element_blank(),
-    #           plot.background = element_rect(fill = "black"),
-    #           panel.background = element_rect(fill = "black"),
-    #           panel.grid = element_blank(),
-    #           legend.text = element_text(colour = "white")
-    #         )
-    #     )
-    #   )
-    # })
   }
   )
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
